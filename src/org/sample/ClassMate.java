66386259c8b52a3d5903926f2b468dd27b712431
Nicky9705 <nickysingh9705@gmail.com>
2022-09-09 18:15:12 +0530

    adding

---
+++ src/org/sample/ClassMate.java
@@ -0,0 +1,35 @@
+package org.sample;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ClassMate {
+	
+	public static void main(String[] args) {
+		
+		Set<Integer> a = new TreeSet <integer>();
+		a.add(10);
+		a.add(50);n 
+		a.add(80);
+		a.add(90);
+		a.add(20); 
+		a.add(20);
+		a.add(10);
+		a.add(50);
+	
+		System.out.println(a);
+		boolean b = a .contains(90);
+		System.out.println("set contains 90:"+b);
+		 
+		a.remove(80);
+		System.out.println(a);
+		
+		a.clear(10);
+		System.out.println(a);
+		
+		
+	} 
+}
+
+
+ 
